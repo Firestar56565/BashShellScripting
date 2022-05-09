6197d82a14cf591b7f3271b99e2db3be4614e002
Shrek <34037661+Firestar56565@users.noreply.github.com>
2022-05-09 09:09:28 -0700

    Project 1 - I/O
    My first project after Hello World, looking at learning input/output commands.

---
+++ HW1.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+echo "-------Input--------"
+
+read -p 'Student name: ' Q1
+
+echo -e "\n"
+
+read -p 'Student class: ' Q2
+
+echo -e "\n"
+
+read -p 'Student major(s): ' Q3
+
+echo -e "\n"
+
+read -p 'Student interests/hobbies: ' Q4
+
+echo -e "\n"
+
+read -p 'Previous student experience with Linux,Linux Bash Shell scripting, Microsoft security, and Microsoft Powershell: ' Q5
+
+echo -e "\n"
+
+echo "-------Output--------"
+
+echo -e "\n"
+
+echo "Student name: $Q1"
+
+echo -e "\n"
+
+echo "Student class: $Q2"
+
+echo -e "\n"
+
+echo "Student major(s): $Q3"
+
+echo -e "\n"
+
+echo "Student interests/hobbies: $Q4"
+
+echo -e "\n"
+
+echo "Previous student experience with Linux, Linux Bash Shell scripting, Microsoft security, and Microsoft Powershell: $Q5"
+
